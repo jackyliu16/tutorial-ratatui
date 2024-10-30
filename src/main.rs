ceb4459307b3e4950c298d8ffc2e338d711a7cef
jackyliu16 <18922251299@163.com>
2024-10-31 05:57:55 +0800

    feat: remove original and add todo

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
-use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
-    style::Stylize,
-    widgets::Paragraph,
-    DefaultTerminal,
-};
 use std::io;
 
-// 1. in alternate screen
-// 2. enable raw mode to turn off input and output
-// 3. app create backend and terminal to print characters to screen
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{
+        block::{Position, Title},
+        Block, Paragraph, Widget,
+    },
+    DefaultTerminal, Frame,
+};
+
+// 1. Initialize the terminal
+// 2. Run application in loop untils exits signal
+// 3. restore terminal back to state before go into alternative screen
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
-    terminal.clear()?;
-
-    let app_result = run(terminal);
-    // ratatui.restore();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press q to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
-        })?;
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
 
-        // handle keyboard events
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
         }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        todo!()
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        todo!()
     }
 }
