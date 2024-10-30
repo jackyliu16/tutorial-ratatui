4a069d12c5ebee3da8c7b0180905eb076c75f596
jackyliu16 <18922251299@163.com>
2024-10-31 05:17:23 +0800

    feat: run hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal,
+};
+use std::io;
+
+// 1. in alternate screen
+// 2. enable raw mode to turn off input and output
+// 3. app create backend and terminal to print characters to screen
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+
+    let app_result = run(terminal);
+    // ratatui.restore();
+    app_result
+}
+
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new("Hello Ratatui! (press q to quit)")
+                .white()
+                .on_blue();
+            frame.render_widget(greeting, frame.area());
+        })?;
+
+        // handle keyboard events
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
 }
